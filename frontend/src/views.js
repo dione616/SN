63a9f6ad3406ae9d9e85fd5d281e700e4637034c
dione616 <Vetal160199@i.ua>
2020-11-25 00:10:05 +0200

    Initial commit

---
+++ frontend/src/views.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
+export const Header = styled.header`
+  background-color: #282c34;
+  height: 10vh;
+  color: white;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  width: 70vw;
+  margin: 0 auto;
+  justify-content: space-between;
+`;
+export const Logo = styled.div`
+  align-self: center;
+  font-family: "Big Shoulders Stencil Display", cursive;
+  font-size: 3rem;
+`;
+export const Auth = styled.div`
+  align-self: center;
+`;
